f795d3682597546efe6607c2e564b8c6320f7535
fatiha <fatiha.al.sid.chikh@etu.imt-nord-europe.fr>
2026-03-18 10:48:52 +0100

    First Commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production:false,
+    contractAddress: '0x79E46b3C57615fB29d6d2E64a1ec81fc1dB43122',
+    chainId: '0x539',
+    ownerAddress:''
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+    production:true,
+    contractAddress: '0x2913AA662181c7B1D5F9C761547EB656788B64C4',
+    chainId: '0xaa36a7',
+    ownerAddress:''
+};
+
